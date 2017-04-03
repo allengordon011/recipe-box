5a396edf569487b0be7e4e169ffb0d441829df7e
trig010 <trig.code@gmail.com>
2017-04-03 17:32:26 -0500

    initial commit

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,23 @@
 import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
+
+const App = () => (
+  <MuiThemeProvider>
+    <MyAwesomeReactComponent />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
